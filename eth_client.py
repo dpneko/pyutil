b836eff274fd9402e0b2c25af6b6fa4812575fbd
Cooper Xiang <Cooper.xiang@tron.network>
2022-09-09 19:01:48 +0800

    eth_client

--- eth_client.py
+++ eth_client.py
@@ -3,6 +3,7 @@ import requests
 import pathos
 import tqdm
 import os
+from web3 import Web3, HTTPProvider
 
 bsc = "https://snowy-boldest-asphalt.bsc.discover.quiknode.pro/560ab43510e2c564d4d363d1ce6b59944b3cb56c/"
 eth = "https://mainnet.infura.io/v3/5f7b1e325e3d46cb8867a7b496365dea"
@@ -33,12 +34,12 @@ def get_pancake_user():
     cores = pathos.multiprocessing.cpu_count()
     pool = pathos.pools.ProcessPool(cores)
 
-    users = set()
-    address = "0x45c54210128a065de780C4B0Df3d16664f7f859e"
-    topics = ["0x2b943276e5d747f6f7dd46d3b880d8874cb8d6b9b88ca1903990a2738e7dc7a1"] # lock
+    users = {}
     fromBlock = 17050265
     toBlock = 21150960
     def func(start):
+        address = "0x45c54210128a065de780C4B0Df3d16664f7f859e"
+        topics = ["0x2b943276e5d747f6f7dd46d3b880d8874cb8d6b9b88ca1903990a2738e7dc7a1"] # lock
         users = {}
         logs = get_logs(address, topics, start, start+5000)
         for log in logs:
@@ -49,10 +50,11 @@ def get_pancake_user():
         return users
 
 
-    for ret in tqdm.tqdm(pool.map(func, range(fromBlock, toBlock, 5000)), total= (toBlock-fromBlock)//5000):
+    for ret in tqdm.tqdm(pool.map(func, list(range(fromBlock, toBlock, 5000)))):
+        # print(ret)
         users.update(ret)
     with open("pancake_user.json", 'w') as f:
-        json.dump(users, f)
+        json.dump(users, f, indent=1)
     return users
 
 if __name__ == '__main__':
