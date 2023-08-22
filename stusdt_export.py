d7d388832af5fb677a4cad8a40df3b6e3c520b6d
Cooper Xiang <cooper.xiang@tron.network>
2023-08-22 17:26:39 +0800

    update stusdt_export

--- stusdt_export.py
+++ stusdt_export.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 from io import StringIO
 import requests
 import wallet
+from os.path import sep
 
 def format_day(timestamp):
     date = datetime.datetime.fromtimestamp(timestamp/1000)
@@ -13,52 +14,55 @@ def format_day(timestamp):
 
 def format_date(timestamp):
     date = datetime.datetime.fromtimestamp(timestamp/1000)
-    return date.strftime("%d/%m/%y %H:%M")
+    return date.strftime("%d/%m/%Y %H:%M")
 
 
 def export_deposit():
     """
     select user_address, underlying_amount, timestamp, tx_id from stusdt_transfer_log
-    where action_type = 1 and timestamp >= 1690819200000 and timestamp < 1691942400000
+    where action_type = 1 and timestamp >= 1690819200000
     order by timestamp asc ;
     """
 
-    raw = pd.read_csv("deposit.csv", sep='\t')
+    raw = pd.read_csv("stusdt_export" + sep + "deposit.csv", sep='\t',
+                       names=['user_address','underlying_amount','timestamp','tx_id'])
 
     raw['day'] = raw['timestamp'].apply(format_day)
     raw['date'] = raw['timestamp'].apply(format_date)
 
     export = raw[['user_address', 'underlying_amount', 'tx_id', 'date', 'day']]
-    print(export)
+    # print(export)
 
-    export.to_csv("deposit_export.csv", float_format='%g', index=False)
+    export.to_csv("stusdt_export" + sep + "deposit_export.csv", float_format='%g', index=False)
 
 def export_unstake():
     """
     select user_address, withdraw_amount, withdraw_fee, request_timestamp, request_tx from stusdt_withdraw_log
-    where status in (0,1,2,3) and request_timestamp >= 1690819200000 and request_timestamp < 1691942400000
+    where status in (0,1,2,3) and request_timestamp >= 1690819200000
     order by request_timestamp asc ;
     """
 
-    raw = pd.read_csv("unstake.csv", sep='\t')
+    raw = pd.read_csv("stusdt_export" + sep + "unstake.csv", sep='\t',
+                       names = ['user_address', 'withdraw_amount', 'withdraw_fee', 'request_timestamp', 'request_tx'])
 
     raw['day'] = raw['request_timestamp'].apply(format_day)
     raw['date'] = raw['request_timestamp'].apply(format_date)
     raw['without_fee'] = raw['withdraw_amount'] - raw['withdraw_fee']
 
     export = raw[['user_address', 'withdraw_amount', 'withdraw_fee', 'request_tx', 'date', 'day', 'without_fee']]
-    print(export)
+    # print(export)
 
-    export.to_csv("unstake_export.csv", float_format='%g', index=False)
+    export.to_csv("stusdt_export" + sep + "unstake_export.csv", float_format='%g', index=False)
 
 def export_claim():
     """
     select user_address, withdraw_amount, withdraw_fee, claim_timestamp, claim_tx from stusdt_withdraw_log
-    where status in (3) and claim_timestamp >= 1690819200000 and claim_timestamp < 1691942400000
+    where status in (3) and claim_timestamp >= 1690819200000
     order by claim_timestamp asc ;
     """
 
-    raw = pd.read_csv("claim.csv", sep='\t')
+    raw = pd.read_csv("stusdt_export" + sep + "claim.csv", sep='\t',
+                      names=['user_address', 'withdraw_amount', 'withdraw_fee', 'claim_timestamp', 'claim_tx'])
 
     raw['day'] = raw['claim_timestamp'].apply(format_day)
     raw['date'] = raw['claim_timestamp'].apply(format_date)
@@ -66,9 +70,9 @@ def export_claim():
     raw['status'] = '已提取'
 
     export = raw[['user_address', 'withdraw_amount', 'withdraw_fee', 'claim_tx', 'status','date', 'day', 'without_fee']]
-    print(export)
+    # print(export)
 
-    export.to_csv("claim_export.csv", float_format='%g', index=False)
+    export.to_csv("stusdt_export" + sep + "claim_export.csv", float_format='%g', index=False)
 
 
 def top_stusdt_holder(total_stUSDT: str, total_share: str):
@@ -130,6 +134,9 @@ def daily_volume():
     """
     pass
     
+export_deposit()
+export_unstake()
+export_claim()
 
 # top_stusdt_holder('68738510.873545500000000003','68583963461552059965162104')
-daily_snapshot()
+# daily_snapshot()
